a9640b1230a007738f9b857963d5cfb128744c8a
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-05-29 22:35:56 +0530

    consumer added for model user

--- src/main/java/com/vedant/kafka/demo/config/KafkaConfiguration.java
+++ src/main/java/com/vedant/kafka/demo/config/KafkaConfiguration.java
@@ -1,18 +1,22 @@
 package com.vedant.kafka.demo.config;
 
 import com.vedant.kafka.demo.model.User;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
@@ -26,8 +30,24 @@ public class KafkaConfiguration {
     }
 
     @Bean
+    public ConsumerFactory<String, User> consumerFactory(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(User.class));
+    }
+
+    @Bean
     public KafkaTemplate<String, User> kafkaTemplate(){
         return new KafkaTemplate<>(producerFactory());
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, User> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
---
+++ src/main/java/com/vedant/kafka/demo/listener/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.vedant.kafka.demo.listener;
+
+import com.vedant.kafka.demo.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test", groupId = "group_id")
+    public void consume(User user){
+        System.out.println("ConsumedMessage: "+user);
+    }
+
+}
